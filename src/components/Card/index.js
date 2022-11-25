16ccefae868a734b657dbaa888809e49b0fa52f4
suzannahurst <hurstsuzanna@gmail.com>
2022-11-25 12:29:18 +0000

    change image url

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,13 +3,11 @@ import { Link } from "react-router-dom";
 
 const Card = ({ article }) => {
   const imageUrl =
-    process.env.NODE_ENV !== "development"
-      ? "https://strapi-v1ph.onrender.com/" +
-        article.attributes.image.data.attributes.url
-      : // : "http://localhost:1337" +
-
-        //  : process.env.REACT_APP_BACKEND_URL +
-        article.attributes.image.data.attributes.url;
+    // process.env.NODE_ENV !== "development"
+    //   ? "http://localhost:1337" + article.attributes.image.data.attributes.url
+    //   : // : "http://localhost:1337" +
+    //     //  : process.env.REACT_APP_BACKEND_URL +
+    article.attributes.image.data.attributes.url;
   // console.log("imageURL", imageUrl);
   return (
     <Link to={`/article/${article.attributes.slug}`} className="uk-link-reset">
