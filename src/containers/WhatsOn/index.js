a25811fcfa91c84632d83f95a42796b15b2d8234
suzannahurst <hurstsuzanna@gmail.com>
2023-05-01 22:15:32 +0100

    added events with new design

--- src/containers/WhatsOn/index.js
+++ src/containers/WhatsOn/index.js
@@ -23,66 +23,10 @@ const WhatsOn = () => {
                 tagline={"in the Coney Zoom bar"}
               />
               <main className="whatson">
-                {/* <ReactMarkdown
+                <ReactMarkdown
                   children={zoombar.attributes.content}
                   rehypePlugins={[rehypeRaw]}
-                ></ReactMarkdown>{" "} */}
-
-                <div className="event">
-                  <div className="event-item">
-                    <h3 className="eventTitle">A Magic Trick</h3>
-                    <p className="eventDate">18 May, 7 June & 5 July 2023</p>
-                  </div>
-                  <div className="event-item secondary">
-                    <p className="description">
-                      A gentle yet impactful piece of play, aiming to help spark
-                      a small positive change in your life.
-                    </p>
-                    <a
-                      href="https://youhavefoundconey.net/suzanna/events/a-magic-trick"
-                      class="book"
-                    >
-                      <button class="orange funButton">More info</button>
-                    </a>
-                  </div>
-                </div>
-
-                <div className="event">
-                  <div className="event-item">
-                    <h3 className="eventTitle">The Hidden</h3>
-                    <p className="eventDate">25 May, 21 June & 18 July 2023</p>
-                  </div>
-                  <div className="event-item secondary">
-                    <p className="description">
-                      A live interactive show played primarily over Zoom, set in
-                      a corporate dystopia game-world.
-                    </p>
-                    <a
-                      href="https://youhavefoundconey.net/suzanna/events/test-event"
-                      class="book"
-                    >
-                      <button class="orange funButton">More info</button>
-                    </a>
-                  </div>
-                </div>
-                <div className="event">
-                  <div className="event-item">
-                    <h3 className="eventTitle">Overgrown</h3>
-                    <p className="eventDate">10 June & 11 June 2023</p>
-                  </div>
-                  <div className="event-item secondary">
-                    <p className="description">
-                      A storytelling game exploring our relationship with
-                      nature, presented with The Place, Bedford.
-                    </p>
-                    <a
-                      href="https://youhavefoundconey.net/suzanna/events/overgrown"
-                      class="book"
-                    >
-                      <button class="orange funButton">More info</button>
-                    </a>
-                  </div>
-                </div>
+                ></ReactMarkdown>{" "}
               </main>
               <Rabbit />
               <Footer />
